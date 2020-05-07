55b159bf2c8f1ca8759f1d600ca704b782348354
XieYongHong <18010623010@163.com>
2020-05-07 09:26:50 +0800

    Update index.tsx
    添加注释

--- src/table/index.tsx
+++ src/table/index.tsx
@@ -37,16 +37,16 @@ interface FilterDomProps {
 
 function table<T>(props: MyTabelProps<T>, ref: any):ReactElement {
 
-    const [columns, setColumns] = useState<MyColumns<any>[]>([])
+    const [columns, setColumns] = useState<MyColumns<any>[]>([]) // 表格列的配置描述
 
-    const [defaultColums, setDefaultColums] = useState<MyColumns<any>[]>([])
+    const [defaultColums, setDefaultColums] = useState<MyColumns<any>[]>([]) // 默认表格列的配置描述
 
-    const [filteredInfo, setFilteredInfo] = useState<any>(null)
-    const [sortedInfo, setSortedInfo] = useState<any>(null)
+    const [filteredInfo, setFilteredInfo] = useState<any>(null) // 过滤参数对象
+    const [sortedInfo, setSortedInfo] = useState<any>(null) // 排序参数对象
 
-    const [queryFilterArgs, setQueryFilterArgs] = useState<any>(null)
+    const [queryFilterArgs, setQueryFilterArgs] = useState<any>(null) // 查询参数对象
 
-    const [editDataList,setEditDataList] = useState<any[]>([])
+    const [editDataList,setEditDataList] = useState<any[]>([]) //
 
     useEffect(() => {
 
@@ -60,48 +60,52 @@ function table<T>(props: MyTabelProps<T>, ref: any):ReactElement {
         getEditData: editDataList
     }))
     
-    function handleColumns (data:Array<MyColumns<any>> = []): MyColumns<any>[] { // 处理columns
+    /**
+    * 处理columns
+    * @param data columns的数组
+    */
+    function handleColumns (data:Array<MyColumns<any>> = []): MyColumns<any>[] {
         return data.map( (item: MyColumns<any>) => {
             item.key = item.dataIndex
             const { filters, sorterKey, editData, render, ...extra} = item
 
-            item.render = (value,record) => {
-                const key = record.key
-                const obj = editDataList.find((a: any) => a.key === key)
-                if(editData) {
-                    const name = editData.key || item.key
+//             item.render = (value,record) => {
+//                 const key = record.key
+//                 const obj = editDataList.find((a: any) => a.key === key)
+//                 if(editData) {
+//                     const name = editData.key || item.key
     
-                    const setValues = (value:any) => {
-                        if(obj) {
-                            obj[name] = value
-                            setEditDataList(editDataList)
-                        } else {
-                            setEditDataList([
-                                ...editDataList,
-                                {
-                                    key: key,
-                                    [name]: value
-                                }
-                            ])
-                        }
+//                     const setValues = (value:any) => {
+//                         if(obj) {
+//                             obj[name] = value
+//                             setEditDataList(editDataList)
+//                         } else {
+//                             setEditDataList([
+//                                 ...editDataList,
+//                                 {
+//                                     key: key,
+//                                     [name]: value
+//                                 }
+//                             ])
+//                         }
     
-                        console.log(editDataList);
+//                         console.log(editDataList);
     
-                    }
+//                     }
     
-                    const dom = editData.dom ? editData.dom(setValues) : value
+//                     const dom = editData.dom ? editData.dom(setValues) : value
                     
-                    return dom
-                }
-            }
+//                     return dom
+//                 }
+//             }
 
-            if(filters && JSON.stringify(filters) !== '{}') { // 返回过滤
+            if(filters && JSON.stringify(filters) !== '{}') { // 自定义过滤
                 return {
                     ...extra,
                     filterDropdown: (a: any) => handleFilter(a, filters, item.dataIndex), 
-                    filteredValue: filteredInfo ? filteredInfo[item.dataIndex as string] : null,
+                    filteredValue: filteredInfo ? filteredInfo[item.dataIndex as string] : null, //控制过滤状态
                     filterIcon: filtered => {
-                        return <FilterOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+                        return <FilterOutlined style={{ color: filtered ? '#1890ff' : undefined }} /> 
                     }
                 }
             }
@@ -109,17 +113,22 @@ function table<T>(props: MyTabelProps<T>, ref: any):ReactElement {
             if(sorterKey) { // 排序
                 return {
                     ...extra,
-                    sortOrder: sortedInfo ? sortedInfo.columnKey === item.dataIndex && sortedInfo.order : false,
+                    sortOrder: sortedInfo ? sortedInfo.columnKey === item.dataIndex && sortedInfo.order : false, // 控制排序状态
                     sorter: true
                 }
             }
             
-
             return item
         })
     }
 
-    function handleFilter(item: any, filters: filtersProps, dataIndex: any):ReactElement { // 处理过滤条件
+    /**
+    * 处理过滤条件，添加Button
+    * @param item 
+    * @param filters 
+    * @param dataIndex 
+    */     
+    function handleFilter(item: any, filters: filtersProps, dataIndex: any):ReactElement {
         const { setSelectedKeys, selectedKeys, confirm, clearFilters } = item
         
         return <div style={{padding:8}}>
@@ -155,7 +164,13 @@ function table<T>(props: MyTabelProps<T>, ref: any):ReactElement {
             </div>
     }
 
-    function handleFilterDom(filters: filtersProps, setSelectedKeys: any, selectedKeys: any):ReactElement { // 处理过滤dom
+    /**
+    * 处理过滤dom
+    * @param filters 外部传入filters对象
+    * @param setSelectedKeys 设置值，必须为下标为0的数组即：setSelectedKeys([xxx])，才能触发Table组件的onChange事件
+    * @param selectedKeys 数组
+    */
+    function handleFilterDom(filters: filtersProps, setSelectedKeys: any, selectedKeys: any):ReactElement {
         
         const { mode, pla, domData } = filters
 
